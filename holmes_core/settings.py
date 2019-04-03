f3196d69080318a7bcd1ae73cb23e75b0c609403
Jon <jambler24@gmail.com>
2019-04-03 12:12:52 +0200

    style and setting changes, maybe bug

--- holmes_core/settings.py
+++ holmes_core/settings.py
@@ -125,13 +125,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATIC_ROOT = '/Users/panix/iCloud/programs/Holmes/holmes_core/processed/'
+STATIC_ROOT = "/static/"
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "processed"),
-    os.path.join(BASE_DIR, "static"),
     os.path.join(BASE_DIR, "temp"),
+    os.path.join(BASE_DIR, "static"),
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
